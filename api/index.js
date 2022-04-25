67bbfbd565b8b17390fcdc7435f1baa3aa217123
unknown <Kirill508017@mail.ru>
2022-04-25 23:38:55 +0300

    add create table in index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import router from "./router/router.js";
 import db from "./db/dbPGSQL.js";
 
@@ -15,7 +15,14 @@ async function startApp() {
     app.listen(PORT, () =>
       console.log(`SERVER STARTED at http://localhost:${PORT}`)
     );
-    await db.query('create TABLE IF NOT EXISTS notes(id SERIAL PRIMARY KEY,title VARCHAR(255),done BOOLEAN,date DATE);');
+    await db.query(
+      "create TABLE IF NOT EXISTS notes(id SERIAL PRIMARY KEY,title VARCHAR(255),done BOOLEAN,date DATE);"
+    );
+
+    await db.query(
+      "insert into notes (title, done, date) values ($1,$2,$3) RETURNING *",
+      ["Тестовая заметка", false, "2022-02-25"]
+    );
   } catch (e) {
     console.log(e);
   }
